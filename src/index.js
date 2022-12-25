d1797c9b8e791fb981ca4b1cf43593c65cb925f5
cristobal-1 <cristobal1@gmail.com>
2022-12-25 17:11:20 -0300

    fix-cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+//cors
+app.use(cors());
+
+//settings
+app.set('port',process.env.PORT || 4000);
+app.set('json spaces',2);
 
 //middlewares
+app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 //Routes
 app.use(require('./routes/index'));
 
-app.listen(4000);
-console.log('server on port 4000');
+//starting the server
+app.listen(app.get('port'),()=>{
+    console.log(`server on port ${app.get('port')}`);
+})
